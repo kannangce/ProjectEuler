5bd0a11a10fb8585595bcd759d075e7982635f74
Kannan Ramamoorthy <kannan.r@imaginea.com>
2018-09-15 23:34:44 +0530

    Removed unused code. Added comments and tests.

--- src/project_euler/5-smallest-multiple.clj
+++ src/project_euler/5-smallest-multiple.clj
@@ -1,7 +1,5 @@
-(defn factors-of-lcm
-  [& numbers])
-
 (defn remove-factors
+  "Remove the given factors in all the numbers of the given sequence"
   [factor seq]
   (if (= 1 factor)
    seq
@@ -11,20 +9,33 @@
          %1) ;; not a factor)
      seq)))
 
-(def lcm-for (vec (range 1 11)))
 
-(loop [lcd 1 remaining lcm-for]
+(defn lcm
+  "Finds the lcm of the numbers in the given sequece"
+  [lcm-for]
+  (loop [lcd 1 remaining lcm-for]
+    (if (= 0 (count remaining))
+      lcd
+      (do
+        (def curr-number (first remaining))
+        (def lcd curr-number)
+        (def multiplier
+          (if (= 0 (mod lcd curr-number))
+            1
+            curr-number))
 
-  (if (= 0 (count remaining))
-    lcd
-    (do
-      (def curr-number (first remaining))
-      (def lcd curr-number)
-      (def multiplier
-        (if (= 0 (mod lcd curr-number))
-          1
-          curr-number))
+        (recur
+            (* lcd curr-number)
+            (rest (remove-factors (first remaining) remaining)))))))
 
-      (recur
-          (* lcd curr-number)
-          (rest (remove-factors (first remaining) remaining))))))
+; LCM of the numbers from 1 to 20
+(lcm (range 1 21))
+
+;; All of the below expected to give true
+(= 182 (lcm [2 91]))
+
+(= 11 (lcm [1 11]))
+
+(= 143 (lcm [13 11]))
+
+(= 16 (lcm [2 4 8 16]))
