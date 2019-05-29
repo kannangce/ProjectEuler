40caa52c43adcac3568ac9e5ae0d7e717b5e6468
Kannan Ramamoorthy <kannan.r@imaginea.com>
2019-05-29 20:44:39 +0530

    Fixed solution

--- src/project_euler/31-coin-combinations.clj
+++ src/project_euler/31-coin-combinations.clj
@@ -1,36 +1,56 @@
-;; THIS IS A IN PROGRESS CODE. YET TO BE COMPLETED
+;; TODO Scope for memoization
+(defn total-val
+  [coins denominations]
+  (reduce + (map * coins denominations)))
+
+(defn max-constraint
+  [expected-val coins-list]
+  (fn [coin-indx coin-cnt]
+    (>= expected-val
+      (* coin-cnt (coins-list coin-indx)))))
+
+(defn next-possible?
+  [expected-val denominations coin-list]
+  (let [max-coin-indx (dec (count coin-list))]
+    ((max-constraint expected-val coin-list)
+     max-coin-indx
+     (denominations max-coin-indx))))
+
+(defn next-possibility
+  [coin-denom max-constraint]
+  (loop [curr-denomiation coin-denom curr-index 0]
+    (let [curr-coin-cnt (coin-denom curr-index)]
+      (if (max-constraint curr-index curr-coin-cnt)
+        (update curr-denomiation curr-index inc)
+        (recur (assoc curr-denomiation curr-index 0) (inc curr-index))))))
 
 (defn all-denominations
   [final-value coins]
   (let [final-value final-value
         coins coins
         max-denom-count (quot final-value (apply min coins))]
-    (loop [all-denomiations []
-           curr-coin-indx 0
-           curr-cnt 0
-           curr-denominations (conj [] curr-cnt)]
-
-      (println (str all-denomiations "-" curr-coin-indx "-" curr-cnt "-" curr-denominations))
-
-      (let [curr-total-val (total-val coins curr-denominations)
-            is-last-coin (= curr-coin-indx (dec (count coins)))]
-        (println (str curr-total-val "-" is-last-coin))
-        (if (and is-last-coin (> curr-cnt max-denom-count))
-          all-denomiations
-          (recur
-            (if (= final-value curr-total-val) (into all-denomiations curr-denominations) all-denomiations)
-            (if (> curr-cnt max-denom-count) (inc curr-coin-indx) curr-coin-indx)
-            (if (> curr-cnt max-denom-count) 0 (inc curr-cnt))
-            (if (> curr-cnt max-denom-count)
-              (conj curr-denominations 0)
-              (conj
-                (into [] (butlast curr-denominations))
-                (inc curr-cnt)))))))))
+    (loop [all-denomiations (vector)
+           curr-denominations (vec (replicate (count coins) 0))]
+          (if-not (next-possible? final-value curr-denominations coins)
+            all-denomiations
+            (recur
+              (if (= final-value (total-val coins curr-denominations))
+                  (conj all-denomiations curr-denominations)
+                  all-denomiations)
+              (next-possibility
+                curr-denominations
+                (max-constraint final-value coins)))))))
 
 
-;; TODO Scope for memoization
-(defn total-val
-  [coins denominations]
-  (reduce + (map * coins denominations)))
+(time (count (all-denominations 200 [1 2])))
+; "Elapsed time: 135.465971 msecs"
+; user=>
+; 101
 
-(all-denominations 10 [1 2])
+(time (count (all-denominations 200 [1 2 5])))
+; "Elapsed time: 7437.966924 msecs"
+; user=>
+; 2081
+
+(time (all-denominations 200 [1 2 5 10 20 50 100 200]))
+; Runs forever
